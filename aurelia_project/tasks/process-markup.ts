09dd0e6b75f747d50bdbfa1124e6326de3693df5
Luke Larsen <luke@lukelarsen.com>
2020-05-19 16:36:46 -0600

    remove fill="currentColor" _facepalm_. add it a better way.

--- aurelia_project/tasks/process-markup.ts
+++ aurelia_project/tasks/process-markup.ts
@@ -3,16 +3,24 @@ import * as project from '../aurelia.json';
 import * as htmlmin from 'gulp-htmlmin';
 import * as svgstore from 'gulp-svgstore';
 import * as inject from 'gulp-inject';
+import * as cheerio from 'gulp-cheerio';
 
 const fileContents = (_filePath, file) => {
-  return file.contents.toString().replace(/\<svg /g, '<svg display="none" ');
+  return file.contents.toString();
 };
 
 export function pluginMarkup(dest) {
   return function processPluginMarkup() {
     const svgs = gulp
       .src('src/svgs/**/*.svg')
-      .pipe(svgstore({ inlineSvg: true }));
+      .pipe(svgstore({ inlineSvg: true }))
+      .pipe(cheerio({
+        run: function ($) {
+          $('path').attr('fill', 'currentColor');
+          $('svg').attr('style', 'display:none');
+        },
+        parserOptions: { xmlMode: true }
+      }));
 
     return gulp.src(project.plugin.source.html)
       .pipe(inject(svgs, { transform: fileContents }))
